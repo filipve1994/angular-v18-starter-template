90e55f12e3e338404770ed51140d39fcce9ecf9d
Filip Vanden Eynde <47209284+filipve1994@users.noreply.github.com>
2024-06-27 11:12:50 +0200

    add transloco plus example for multilanguage support

---
+++ links.md
@@ -0,0 +1,15 @@
+# links and tips
+
+## links
+
+- https://www.codeandweb.com/babeledit/tutorials/how-to-translate-your-angular-app-with-transloco
+- 
+
+
+## tips
+
+### generate environments file back again
+
+```
+ng g environments
+```
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TranslocoDirective, TranslocoPipe, TranslocoService} from "@jsverse/transloco";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslocoPipe, TranslocoDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'angular-v18-starter-template';
+
+  constructor(private translocoService: TranslocoService) {
+  }
+
+  switchLanguage(lang:string) {
+    this.translocoService.setActiveLang(lang);
+  }
 }
